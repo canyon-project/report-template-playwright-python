823064e11660cc05b71e89cabc31f28187ba9d51
Travis Zhang <wr.zhang25@gmail.com>
2026-03-24 13:58:41 +0800

    feat: update

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,8 @@
-def test_homepage(page):
-    page.goto("http://localhost:5173")
-    assert "react" in page.title()
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title(page: Page):
+    page.goto("http://localhost:5173/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("react"))
